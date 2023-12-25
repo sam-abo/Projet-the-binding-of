c2e4b3c3cae785a3bdb49c51cec12660b6bb0abf
samuel <samuelavah@gmail.com>
2023-12-25 16:11:44 +0100

    version mergée, tout est bien pour une nouvelle base
    affichage géré par une classe, touches aussi
    salles changées, idée de matrices etc...
    déplacement du main dans la classe game

--- Game.cc
+++ Game.cc
@@ -6,64 +6,78 @@ void Game :: jouer(){
     unsigned int screenHeight = desktopMode.height;
 
     // Creation de la fenêtre SFML
-    //sf::RenderWindow window(sf::VideoMode(screenWidth-100, screenHeight-100), "The binding of chatelêt les halles"); //dans la fonction affichage
     Afficher jeu;
     jeu.Fenetre_jeu("The binding of");
-    salle s1(screenWidth-100, screenHeight-100, sf::Color::Red,3,3);
-
-    salle s2(screenWidth-100, screenHeight-100, sf::Color::Blue,3,3);
-
-    // Declaration de la salle active (initialisee à la première salle)
-    salle* salleActive = &s1;
-
-    // Creation de l'entite 1 : le perso principal
-    Hero entite1(50.0f, sf::Color::Green, 200.0f, 100.0f,1.0f);
-
-    std::vector<Objet> entities;
-
-    // Creation du 2eme truc via Objet
-    entities.push_back(Entity(30.0f, sf::Color::Blue, 800.0f, 500.0f,&s1));
-    entities.push_back(Objet(30.0f, sf::Color::Yellow, 500.0f, 800.0f,&s1));
     Touches key;
 
-    // Boucle principale (à mettre dans une classe jeu)
+
+    //"matrice des portes" on indique où seront les portes dans chaques salles, mais en soit ça décrit les propriétés de la salle,
+    //c'est une matrice des propriétés des salles
+    std::vector<std::vector<std::pair<std::string, std::string>>> matrix = {
+        {{"bas", "droite"},{"bas", "gauchedroite"}, {"bas", "gauche"}},
+        {{"hautbas", "droite"},{"hautbas", "gauchedroite"}, {"hautbas", "gauche"}},
+        {{"haut", "droite"},{"haut", "gauchedroite"}, {"haut", "gauche"}}
+    };
+
+    //créer une carte qui est associée à une matrice, ici la matrice est définie au dessus, les 2 derniers trucs en paramètres sont là pour la taille des salles
+    carte carte(matrix,screenWidth-100, screenHeight-100);
+    
+    
+
+    // Creation de l'entite 1 : le perso principal
+    Hero hero(screenWidth/100.0f, sf::Color::Green, 200.0f, 100.0f,1.0f);
+    
+    //vecteur des entités présentes dans la salle
+    std::vector<Objet> entities;
+    //on déclare des entités et on spécifie dans quelle salle de la matrice de la carte associée elles sont. 
+    //Peut être que c'est déplacable pour faire un truc plus élégant
+    entities.push_back(Entity(30.0f, sf::Color::Blue, 800.0f, 500.0f,carte.getsalleActive()));
+    entities.push_back(Objet(30.0f, sf::Color::Yellow, 500.0f, 800.0f,carte.getsalleActive()));
+    entities.push_back(Objet(30.0f, sf::Color::Yellow, 500.0f, 800.0f,&carte.getgrille()[1][1]));
+
+
+
+    // Boucle principale
     while (jeu.getWindow().isOpen()) {
         // Gestion des evenements
+        jeu.fermeture(key);
         sf::Event event;
         while (jeu.getWindow().pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 jeu.getWindow().close();
             }
         }
+        
+        sf::Vector2f prevPositionEntity1 = hero.getforme().getPosition();
+        
+
+        hero.mouvement(key);
+        
+        //ça ira dans une fonction dédiée dans entité.
+        //Gestion des collisions avec les murs de la salle active.
+        hero.collision(carte.getsalleActive()->Getmgauche().getGlobalBounds(),prevPositionEntity1);
+        hero.collision(carte.getsalleActive()->Getmdroite().getGlobalBounds(),prevPositionEntity1);
+        hero.collision(carte.getsalleActive()->Getmhaut().getGlobalBounds(),prevPositionEntity1);
+        hero.collision(carte.getsalleActive()->Getmbas().getGlobalBounds(),prevPositionEntity1);
+
+        //la fonction qui dit "si le machin passé en paramètre touches telle ou telle porte, il change de salle"
+        carte.deplacementEntreSalle(&hero);
+        
+        
         jeu.getWindow().clear();
-        jeu.fermeture(key);
-        // Enregistrement de la position precedente de l'entite 1
-        sf::Vector2f prevPositionEntity1 = entite1.getforme().getPosition();
-
-        entite1.mouvement(key);
-        entite1.collision(s1.Getw1().getGlobalBounds(),prevPositionEntity1);
-        entite1.collision(s1.Getw2().getGlobalBounds(),prevPositionEntity1);
-        entite1.collision(s1.Getw3().getGlobalBounds(),prevPositionEntity1);
-        entite1.collision(s1.Getw4().getGlobalBounds(),prevPositionEntity1);
-
-
-        // Verification de la frontière de la première salle
-        entite1.changersalle(salleActive,s2,"gauche");
-        entite1.changersalle(salleActive,s1,"droite");
         
-        
-        // Dessinez la salle active
-        jeu.dessiner_salle(salleActive);
+        //afficher la salle active
+        jeu.dessiner_salle(carte.getsalleActive());
+        //afficher le hero
+        jeu.dessiner_obj(hero);
+        //afficher les hp du hero
+        jeu.afficherHP(hero);
 
-        //entite1.dessiner(window);
-        jeu.dessiner_obj(entite1);
-        //entite1.afficherHP(window);
-        jeu.afficherHP(entite1);
-
+        //boucle for qui gère l'affichage de toutes les entités présentes dans la salle active
         for (Objet& entite : entities) {
-        if (entite.getSalleAppartenance() == salleActive) {
+        if (entite.getSalleAppartenance() == carte.getsalleActive()) {
             jeu.dessiner_obj(entite);
-            entite1.collision(entite.getGlobalBounds(),prevPositionEntity1);
+            hero.collision(entite.getGlobalBounds(),prevPositionEntity1);
         }
         
     }
